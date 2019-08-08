3ac97adebb9fa84073d603a9e5155093e4482c28
andoid-sometime <53864862+andoid-sometime@users.noreply.github.com>
2019-08-08 15:42:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/chargeForCookie', async (request, response) => {
            name: "Cookie 🍪",
            quantity: "1",
            base_price_money: {
-           amount: 100,
+           amount: requestBody.amount,
           currency: "USD"
           }
          // name:requestBody.name,
